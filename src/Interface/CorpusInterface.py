be8bb16fd40514210d00cebd7501e651cd8735f2
Shaamallow <eyal.benaroche@gmail.com>
2022-12-31 13:27:50 +0100

    Comment coprus TAB code

--- src/Interface/CorpusInterface.py
+++ src/Interface/CorpusInterface.py
@@ -8,18 +8,29 @@ import os
 from src.Interface.ClassObjects.CorpusClass import Corpus
 
 class CorpusInterface(object):
+    """Corpus Interface 
+
+    PARAMETERS : 
+    - path : path to the corpus folder
+    - corpus_list : list of all available corpus
+    - corpus : current selected corpus object
+    - corpus_path : path to the current selected corpus
+
+    METHODS : 
+    - Interface : create and return a gradio component for the corpus TAB
+    """
 
     def __init__(self, path, corpus_list):
         self.path = path
-        self.corpus_list = corpus_list # corpus object from Corpus.py
+        self.corpus_list = corpus_list # all available corpus
         self.corpus = None # current selected corpus object
         self.corpus_path = None # path to the current selected corpus
     
     # use block to create a gradio component
 
     def Interface(self):
-        # create a gradio block for corpus 
-        # return the corpus block 
+        """
+        Return a gradio component for the corpus"""
         with gr.Blocks() as corpus:
             gr.Markdown("## Corpus Loader")
 
@@ -39,18 +50,27 @@ class CorpusInterface(object):
             corpus_description = gr.Textbox(label="Description", lines=5)
 
             # Define button behavior after all the components are created
-            load_corpus.click(self.LoadCorpus, inputs=[current_corpus], outputs=[corpus_size,corpus_description])
+            load_corpus.click(self.__LoadCorpus, inputs=[current_corpus], outputs=[corpus_size,corpus_description])
 
         return corpus
 
     # Manualy modify corpus, add an other function to modify the selected corpus from the gradio app
-    def Manual_LoadCorpus(self, corpus):
+    def __Manual_LoadCorpus(self, corpus):
         # Modify the current corpus object
         self.corpus = corpus
 
-    def LoadCorpus(self, corpus_name):
-        # Load the corpus object
-        # corpus_name is a Dropwdown object with the name of the corpus
+    # Modify the function with the elemlents to display in the gradio app
+    def __LoadCorpus(self, corpus_name):
+        """
+        Call by the gradio button, MODIFY the current corpus object
+        Return the corresponding useful elements to display in the gradio app
+
+        PARAMETERS :
+        - corpus_name : name of the corpus to load
+        
+        RETURN : 
+        - size of the corpus
+        - description of the corpus"""
 
         self.corpus = Corpus(self.path+'/'+corpus_name)
 
