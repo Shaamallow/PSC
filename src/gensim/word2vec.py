cbc4e003a0c2d3e34ca67dffaec964a86fb31ea7
Shaamallow <eyal.benaroche@gmail.com>
2023-03-16 10:39:34 +0100

    Clustering on corpus

--- src/gensim/word2vec.py
+++ src/gensim/word2vec.py
@@ -10,8 +10,46 @@ import gensim as gm
 wv = gm.models.KeyedVectors.load_word2vec_format("./data/pre-trained-models/word2vec-google-news-300/GoogleNews-vectors-negative300.bin", binary=True)
 
 
-# Retrieve Voc
-for index, word in enumerate(wv.index_to_key):
-    if index == 10:
-        break
-    print(f"word #{index}/{len(wv.index_to_key)} is {word}")
+from sklearn.decomposition import IncrementalPCA    # inital reduction
+from sklearn.manifold import TSNE                   # final reduction
+import numpy as np                                  # array handling
+import matplotlib.pyplot as plt
+import random
+
+
+def reduce_dimensions(wv):
+    num_dimensions = 2  # final num dimensions (2D, 3D, etc)
+
+    # extract the words & their vectors, as numpy arrays
+    vectors = np.asarray(wv.vectors)
+    labels = np.asarray(wv.index_to_key)  # fixed-width numpy strings
+
+    # reduce using t-SNE
+    tsne = TSNE(n_components=num_dimensions, random_state=0)
+    vectors = tsne.fit_transform(vectors)
+
+    x_vals = [v[0] for v in vectors]
+    y_vals = [v[1] for v in vectors]
+    return x_vals, y_vals, labels
+
+
+x_vals, y_vals, labels = reduce_dimensions(wv)
+
+
+def plot_with_matplotlib(x_vals, y_vals, labels):
+    
+
+    random.seed(0)
+
+    plt.figure(figsize=(12, 12))
+    plt.scatter(x_vals, y_vals)
+
+    #
+    # Label randomly subsampled 25 data points
+    #
+    indices = list(range(len(labels)))
+    selected_indices = random.sample(indices, 25)
+    for i in selected_indices:
+        plt.annotate(labels[i], (x_vals[i], y_vals[i]))
+
+plot_with_matplotlib(x_vals, y_vals, labels)
